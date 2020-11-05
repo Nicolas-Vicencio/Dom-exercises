8a8895878d60e40912f440794cc1bfca8dfb10a8
Nicolas <nicolas.vic.or@protonmail.com>
2020-11-04 21:29:31 -0300

    dag

--- js/Digital_draw.js
+++ js/Digital_draw.js
@@ -3,10 +3,14 @@ const d = document
 export default function digitalDraw(elements, btn){
   const $el = d.querySelectorAll(elements)
   console.log($el);
-  
+  const draw = () => {
+    let winner = Math.floor(Math.random() * $el.length )
+    
+    alert(`The winner is ${$el[winner].innerHTML}!!!`)
+  }
 d.addEventListener('click', e =>{
   if(e.target.matches(btn)){
-
+    draw()
   }
 })
 
